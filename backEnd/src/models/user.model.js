69ef65157e26547d795c24e1f8fe9f6ddabfdae4
Josue Zelada <00053722@uca.edu.sv>
2024-06-24 21:15:54 -0600

    Add: user model

---
+++ backEnd/src/models/user.model.js
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{ 
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastName:{
+        type: String,
+        required: true,
+        trim: true
+
+    },
+    email: { 
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    role:{
+        type : String,
+        required: true, 
+        enum: ["admin", "user"],
+          default: "user",
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+},{
+    timestamps: true
+})
+
+export default mongoose.model('User', userSchema)
